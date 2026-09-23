e302da4e682208abbaa58d0bee0a58cf60df513b
bovevo <solutions@bovevo.com>
2026-09-23 11:51:57 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,964 @@
+const PRODUCTS = [
+ {
+  "id": 7936688259318,
+  "col": "flas-indirimler",
+  "t": "ORZAX Cosakondrin MSM 60 Tablet",
+  "b": "Orzax",
+  "p": 829.9,
+  "o": 949.9,
+  "img": "img/b259aee94c23.png",
+  "bd": [],
+  "tags": [
+   "Vitaminler"
+  ]
+ },
+ {
+  "id": 9539701997814,
+  "col": "flas-indirimler",
+  "t": "PROTEİNOCEAN Mass Gainer Chocolate Hazelnut 2500g",
+  "b": "ProteinOcean",
+  "p": 1499.0,
+  "o": 2299.0,
+  "img": "img/f0a2f8b96072.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri"
+  ]
+ },
+ {
+  "id": 9524062486774,
+  "col": "flas-indirimler",
+  "t": "PROTEİNOCEAN Cream Of Rice Peanut Chocolate 1000g",
+  "b": "ProteinOcean",
+  "p": 299.0,
+  "o": 399.0,
+  "img": "img/d600dde60207.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8198518407414,
+  "col": "flas-indirimler",
+  "t": "SCHAR Petibör Klasik Bisküvi 165g",
+  "b": "Schar",
+  "p": 324.9,
+  "o": 394.9,
+  "img": "img/aab8180ce625.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 9408384925942,
+  "col": "flas-indirimler",
+  "t": "HARDLİNE %100 Whey Protein Çikolata 400g",
+  "b": "Hardline",
+  "p": 1349.1,
+  "o": 1729.0,
+  "img": "img/d437ec665599.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri"
+  ]
+ },
+ {
+  "id": 9479608140022,
+  "col": "flas-indirimler",
+  "t": "HARDLİNE Çikolatalı Protein Bar 45g",
+  "b": "Hardline",
+  "p": 134.1,
+  "o": 199.0,
+  "img": "img/737dce3de451.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri"
+  ]
+ },
+ {
+  "id": 7890086363382,
+  "col": "flas-indirimler",
+  "t": "SOLGAR Calcium Magnesium Ve Vitamin D3 150 Tablet",
+  "b": "Solgar",
+  "p": 949.9,
+  "o": 1094.9,
+  "img": "img/9a97fad22136.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vitaminler"
+  ]
+ },
+ {
+  "id": 9407837634806,
+  "col": "flas-indirimler",
+  "t": "HARDLİNE %100 Whey Protein Çikolata 1600g",
+  "b": "Hardline",
+  "p": 3779.1,
+  "o": 4829.0,
+  "img": "img/6ce24d562a95.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri"
+  ]
+ },
+ {
+  "id": 9437065314550,
+  "col": "flas-indirimler",
+  "t": "KEKESHİ Rice Noodle Pirinç Çubuğu 500g",
+  "b": "Kekeshi",
+  "p": 239.9,
+  "o": 274.9,
+  "img": "img/367ce06b47d1.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8802572959990,
+  "col": "flas-indirimler",
+  "t": "SWİSS BORK CABB 30 Kapsül",
+  "b": "Bork",
+  "p": 899.9,
+  "o": 1099.9,
+  "img": "img/caaba4f75721.png",
+  "bd": [
+   "VG"
+  ],
+  "tags": [
+   "Vegan Ürünler",
+   "Vitaminler"
+  ]
+ },
+ {
+  "id": 8015573811446,
+  "col": "flas-indirimler",
+  "t": "BEYORGANİK Organik Mom's Mix 315g",
+  "b": "Beyorganik",
+  "p": 359.9,
+  "o": 465.9,
+  "img": "img/8dbe3bdc342e.png",
+  "bd": [
+   "VG"
+  ],
+  "tags": [
+   "Anne ve Bebek",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 9478682345718,
+  "col": "flas-indirimler",
+  "t": "HARDLİNE %100 Whey Protein Çilek 1600g",
+  "b": "Hardline",
+  "p": 3779.1,
+  "o": 4199.0,
+  "img": "img/9d110e416f74.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri"
+  ]
+ },
+ {
+  "id": 8041066201334,
+  "col": "bitkisel-protein",
+  "t": "NUSTİL Bal Kabağı Çekirdeği Proteini Tozu 350g",
+  "b": "Nustil Nutrition Style",
+  "p": 599.9,
+  "o": null,
+  "img": "img/266f5aed6f86.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 9109535949046,
+  "col": "bitkisel-protein",
+  "t": "PROTEINOCEAN Pea Protein Çikolata 400g",
+  "b": "ProteinOcean",
+  "p": 628.9,
+  "o": null,
+  "img": "img/cb125c254f93.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 9109528150262,
+  "col": "bitkisel-protein",
+  "t": "PROTEINOCEAN Pea Protein Cookie&Banana 400g",
+  "b": "ProteinOcean",
+  "p": 628.9,
+  "o": null,
+  "img": "img/fb6cff22495c.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 6866438914216,
+  "col": "bitkisel-protein",
+  "t": "SAF Protein Superfood Mix Sports 360g",
+  "b": "Saf Nutrition",
+  "p": 1159.0,
+  "o": null,
+  "img": "img/28c60a37b136.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 7950193459446,
+  "col": "bitkisel-protein",
+  "t": "NATURİGA Berry Protein Karışımı(Berry Blend) 250g",
+  "b": "Naturiga",
+  "p": 825.0,
+  "o": null,
+  "img": "img/2d232d5841b4.png",
+  "bd": [
+   "VG"
+  ],
+  "tags": [
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 9109558460662,
+  "col": "bitkisel-protein",
+  "t": "PROTEINOCEAN Pea Protein Çikolata 1600g",
+  "b": "ProteinOcean",
+  "p": 1993.9,
+  "o": null,
+  "img": "img/47a29f14245c.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8078533951734,
+  "col": "bitkisel-protein",
+  "t": "HARDLINE Vmax Bitkisel Protein Çikolata 908g",
+  "b": "Hardline",
+  "p": 1466.1,
+  "o": 1629.0,
+  "img": "img/411b8e84d2b6.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 7900207055094,
+  "col": "bitkisel-protein",
+  "t": "VEGGY Bezelye Proteini 200g",
+  "b": "Veggy",
+  "p": 399.9,
+  "o": null,
+  "img": "img/35236c1a7b0c.png",
+  "bd": [
+   "VG"
+  ],
+  "tags": [
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 7950192115958,
+  "col": "bitkisel-protein",
+  "t": "NATURİGA Yeşil Protein Karışımı (Green Blend) 250g",
+  "b": "Naturiga",
+  "p": 650.0,
+  "o": null,
+  "img": "img/f45fd7207600.png",
+  "bd": [
+   "VG"
+  ],
+  "tags": [
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 7950171504886,
+  "col": "bitkisel-protein",
+  "t": "NATURİGA Pure Protein Karışımı 250g",
+  "b": "Naturiga",
+  "p": 770.0,
+  "o": null,
+  "img": "img/1db4940ba06e.png",
+  "bd": [
+   "VG",
+   "OR"
+  ],
+  "tags": [
+   "Organik Ürünler",
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 6866436456616,
+  "col": "bitkisel-protein",
+  "t": "SAF Protein Superfood Mix Glow 360g",
+  "b": "Saf Nutrition",
+  "p": 1289.0,
+  "o": null,
+  "img": "img/27d90ce42afc.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 9438477451510,
+  "col": "bitkisel-protein",
+  "t": "WEFOOD Kinoalı Protein Patlak 80g",
+  "b": "Wefood",
+  "p": 74.9,
+  "o": null,
+  "img": "img/23be2577497f.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8772120871158,
+  "col": "glutensiz-cikolatalar",
+  "t": "DELLY Beyaz Çikolata & Kurabiye Aromalı, Çikolata Kaplı, Yüksek Protei",
+  "b": "Delly",
+  "p": 79.5,
+  "o": null,
+  "img": "img/b914afcdc25c.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 7996291383542,
+  "col": "glutensiz-cikolatalar",
+  "t": "DİAS SPECİAL Düşük Proteinli Çikolatalı Gofret 110g",
+  "b": "Dias Special",
+  "p": 145.0,
+  "o": null,
+  "img": "img/c87cba08476a.png",
+  "bd": [
+   "GF",
+   "VG",
+   "LP"
+  ],
+  "tags": [
+   "Düşük Proteinli Ürünler",
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 9016634081526,
+  "col": "glutensiz-cikolatalar",
+  "t": "DELLY Çıtırık Kinoa Patlaklı & Fındıklı Çikolata 40g",
+  "b": "Delly",
+  "p": 76.95,
+  "o": null,
+  "img": "img/833fc808b8ea.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8753916772598,
+  "col": "glutensiz-cikolatalar",
+  "t": "DELLY Çikolata Kaplı Yüksek Proteinli Bar - Karamel Aromalı 40g",
+  "b": "Delly",
+  "p": 79.5,
+  "o": null,
+  "img": "img/30e844c5cacc.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 7952043704566,
+  "col": "glutensiz-cikolatalar",
+  "t": "MAYALI HANE Düşük Proteinli ve Glutensiz Kakaolu Kurabiye 150g",
+  "b": "Mayalı Hane",
+  "p": 220.0,
+  "o": null,
+  "img": "img/a3e1be9c7996.png",
+  "bd": [
+   "GF",
+   "LP"
+  ],
+  "tags": [
+   "Düşük Proteinli Ürünler",
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8755389792502,
+  "col": "glutensiz-cikolatalar",
+  "t": "DELLY Çikolata Kaplı Yüksek Proteinli Bar-Cappuccino Aromalı 40g",
+  "b": "Delly",
+  "p": 79.5,
+  "o": null,
+  "img": "img/1b1d2532ec11.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8742485786870,
+  "col": "glutensiz-cikolatalar",
+  "t": "DELLY Çikolata Kaplı Yüksek Proteinli Bar - Orman Meyveleri Aromalı 40",
+  "b": "Delly",
+  "p": 79.5,
+  "o": null,
+  "img": "img/787cf67602cb.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8755409518838,
+  "col": "glutensiz-cikolatalar",
+  "t": "DELLY Muz Aromalı, Çikolata Kaplı, Yüksek Proteinli Bar 40g",
+  "b": "Delly",
+  "p": 79.5,
+  "o": null,
+  "img": "img/b71e7cd6bc97.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8774598721782,
+  "col": "glutensiz-cikolatalar",
+  "t": "DELLY Çikolata Kaplı Yer Fıstıklı & Vişneli Meyveli Bar 35g",
+  "b": "Delly",
+  "p": 50.95,
+  "o": null,
+  "img": "img/bccda04a3f07.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8774627066102,
+  "col": "glutensiz-cikolatalar",
+  "t": "DELLY Çikolata Kaplı, Bademli & Çilekli Meyveli Bar 35g",
+  "b": "Delly",
+  "p": 50.95,
+  "o": null,
+  "img": "img/b3cca178dca5.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 7995278098678,
+  "col": "glutensiz-cikolatalar",
+  "t": "MAYALI HANE Düşük Proteinli Kurabiyeli Çikolata 35gr",
+  "b": "Mayalı Hane",
+  "p": 130.0,
+  "o": null,
+  "img": "img/6e8666f26f3e.png",
+  "bd": [
+   "GF",
+   "VG",
+   "LP"
+  ],
+  "tags": [
+   "Düşük Proteinli Ürünler",
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 7898612302070,
+  "col": "glutensiz-cikolatalar",
+  "t": "MAYALI HANE Düşük Proteinli Çikolata Parçacıklı Kurabiye 150g",
+  "b": "Mayalı Hane",
+  "p": 220.0,
+  "o": null,
+  "img": "img/7feeed8f401b.png",
+  "bd": [
+   "GF",
+   "VG",
+   "LP"
+  ],
+  "tags": [
+   "Düşük Proteinli Ürünler",
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 6184056815784,
+  "col": "glutensiz-atistirmaliklar",
+  "t": "BALVİTEN Proteini Azaltılmış  Glutensiz Çubuk Kraker 70g",
+  "b": "Balviten",
+  "p": 159.9,
+  "o": null,
+  "img": "img/fbd4764b5b9c.png",
+  "bd": [
+   "GF",
+   "VG",
+   "LP"
+  ],
+  "tags": [
+   "Düşük Proteinli Ürünler",
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 7545544769782,
+  "col": "glutensiz-atistirmaliklar",
+  "t": "NUSTİL Kinoa Cipsi 35g",
+  "b": "Nustil Nutrition Style",
+  "p": 104.9,
+  "o": null,
+  "img": "img/ba34d48a2536.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Anne ve Bebek",
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 9142197813494,
+  "col": "glutensiz-atistirmaliklar",
+  "t": "NUSTİL Karabuğday Patlağı 85g",
+  "b": "Nustil Nutrition Style",
+  "p": 79.9,
+  "o": null,
+  "img": "img/a17d4ca0067d.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 7688315273462,
+  "col": "glutensiz-atistirmaliklar",
+  "t": "NUSTİL Sarımsaklı & Soğanlı Kinoa Cipsi 35g",
+  "b": "Nustil Nutrition Style",
+  "p": 104.9,
+  "o": null,
+  "img": "img/11449ceaadd9.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Anne ve Bebek",
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 9170722586870,
+  "col": "glutensiz-atistirmaliklar",
+  "t": "PROTEİNOCEAN Tiramisu Protein Bar 50g x 4 Adet",
+  "b": "ProteinOcean",
+  "p": 399.9,
+  "o": null,
+  "img": "img/1e3cadd69da2.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Sporcu Ürünleri"
+  ]
+ },
+ {
+  "id": 8019034964214,
+  "col": "glutensiz-atistirmaliklar",
+  "t": "SCHAR Glutensiz Mini Sorrisi Kaymaklı Çikolatalı Bisküvi 100g",
+  "b": "Schar",
+  "p": 234.9,
+  "o": null,
+  "img": "img/c11795755138.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 7545558073590,
+  "col": "glutensiz-atistirmaliklar",
+  "t": "MAYALI HANE Düşük Proteinli ve Glutensiz Pizza Kraker 100g",
+  "b": "Mayalı Hane",
+  "p": 260.0,
+  "o": null,
+  "img": "img/e74fa3c62648.png",
+  "bd": [
+   "GF",
+   "VG",
+   "LP"
+  ],
+  "tags": [
+   "Düşük Proteinli Ürünler",
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 7433100820726,
+  "col": "glutensiz-atistirmaliklar",
+  "t": "ZÜBER %100 Fıstık Ezmesi 315g",
+  "b": "Züber",
+  "p": 399.95,
+  "o": null,
+  "img": "img/252f82e527e1.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8881240473846,
+  "col": "glutensiz-atistirmaliklar",
+  "t": "SCHAR Chocotti Bisküvi 70g",
+  "b": "Schar",
+  "p": 114.9,
+  "o": null,
+  "img": "img/1513feb9d087.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8198399623414,
+  "col": "glutensiz-atistirmaliklar",
+  "t": "SCHAR Nigotti Çörek Otlu Kraker 40g",
+  "b": "Schar",
+  "p": 84.9,
+  "o": null,
+  "img": "img/6e5ddbb27c23.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8364034752758,
+  "col": "glutensiz-ekmekler",
+  "t": "İHE Glutensiz Ekmek 50g (Açıklamayı okuyunuz)",
+  "b": "istanbul Halk Ekmek",
+  "p": 6.9,
+  "o": null,
+  "img": "img/10d61e1efe00.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8364044452086,
+  "col": "glutensiz-ekmekler",
+  "t": "İHE Glutensiz Ekmek 50g x 3 (Açıklamayı okuyunuz)",
+  "b": "istanbul Halk Ekmek",
+  "p": 19.9,
+  "o": null,
+  "img": "img/0e12d304b8e5.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8364046876918,
+  "col": "glutensiz-ekmekler",
+  "t": "İHE Düşük Proteinli Ekmek 50g (Açıklamayı okuyunuz)",
+  "b": "istanbul Halk Ekmek",
+  "p": 6.9,
+  "o": null,
+  "img": "img/7458cda291ca.png",
+  "bd": [
+   "GF",
+   "LP"
+  ],
+  "tags": [
+   "Düşük Proteinli Ürünler",
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8364051202294,
+  "col": "glutensiz-ekmekler",
+  "t": "İHE Düşük Proteinli Ekmek 50g x3 (Açıklamayı okuyunuz)",
+  "b": "istanbul Halk Ekmek",
+  "p": 19.9,
+  "o": null,
+  "img": "img/6690479a6a1f.png",
+  "bd": [
+   "GF",
+   "LP"
+  ],
+  "tags": [
+   "Düşük Proteinli Ürünler",
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8363322114294,
+  "col": "glutensiz-ekmekler",
+  "t": "ROLLA GLUTEN FREE Glutensiz Baton Ekmek - Laktozsuz & Vegan 250g",
+  "b": "Rolla Gluten-Free",
+  "p": 124.9,
+  "o": null,
+  "img": "img/324d9c1eaa42.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8362722951414,
+  "col": "glutensiz-ekmekler",
+  "t": "ROLLA GLUTEN-FREE Glutensiz Jumbo Tost Ekmeği (4x60g)",
+  "b": "Rolla Gluten-Free",
+  "p": 149.9,
+  "o": null,
+  "img": "img/80751af36022.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8362672324854,
+  "col": "glutensiz-ekmekler",
+  "t": "ROLLA GLUTEN-FREE Glutensiz Simit (2 Adet x 100g)",
+  "b": "Rolla Gluten-Free",
+  "p": 169.9,
+  "o": null,
+  "img": "img/9915574c0737.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8243711803638,
+  "col": "glutensiz-ekmekler",
+  "t": "SCHAR Ciabatta Rustica 200g",
+  "b": "Schar",
+  "p": 449.0,
+  "o": null,
+  "img": "img/98a3d84d555c.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8213519368438,
+  "col": "glutensiz-ekmekler",
+  "t": "SCHAR Pane Casereccio - Ev Ekmeği 240g",
+  "b": "Schar",
+  "p": 449.0,
+  "o": null,
+  "img": "img/bbce37caa31d.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8019031195894,
+  "col": "glutensiz-ekmekler",
+  "t": "SCHAR Pan Multigrano Tahıllı Ekmek 250g",
+  "b": "Schar",
+  "p": 179.9,
+  "o": null,
+  "img": "img/b52858482476.png",
+  "bd": [
+   "GF",
+   "VG"
+  ],
+  "tags": [
+   "Glutensiz Ürünler",
+   "Vegan Ürünler"
+  ]
+ },
+ {
+  "id": 8019030638838,
+  "col": "glutensiz-ekmekler",
+  "t": "SCHAR Pan Rustico Glutensiz Çok Tahıllı Ekmek 250g",
+  "b": "Schar",
+  "p": 179.9,
+  "o": null,
+  "img": "img/b41a847e021d.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ },
+ {
+  "id": 8019029950710,
+  "col": "glutensiz-ekmekler",
+  "t": "SCHAR Pan Blanco Glutensiz Dilimli Ekmek 250 g",
+  "b": "Schar",
+  "p": 159.9,
+  "o": null,
+  "img": "img/a0a95fc9804b.png",
+  "bd": [
+   "GF"
+  ],
+  "tags": [
+   "Glutensiz Ürünler"
+  ]
+ }
+];
